ae07aa4ebbc0ea87bc50938302e3d9b9eb595a9f
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-09-25 15:23:47 +0530

    tip calculator

---
+++ tip_calculator/script.js
@@ -0,0 +1,28 @@
+const billInput = document.getElementById('billAmount');
+const tipPercentageInput = document.getElementById('tipPercentage');
+const calculateButton = document.getElementById('calculate');
+const resultOutput = document.getElementById('results');
+
+calculateButton.addEventListener('click', () => {
+  const bill = parseFloat(billInput.value);
+  const tipPercentage = parseFloat(tipPercentageInput.value);
+
+  if (
+    isNaN(bill) ||
+    isNaN(tipPercentage) ||
+    bill === '' ||
+    tipPercentage === ''
+  ) {
+    resultOutput.textContent = 'Please enter the valid numbers';
+  } else {
+    // tip calculate
+
+    const tipAmount = (bill * tipPercentage) / 100;
+    const totalBill = tipAmount + bill;
+    console.log(totalBill); // just for checking the logic
+
+    // content to display
+
+    resultOutput.textContent = `Total Bill (including tip): $${totalBill}`;
+  }
+});
